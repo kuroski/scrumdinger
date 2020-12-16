a4f5418704199bdc3dd4df7a1b805720c126d71d
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-12-16 17:47:57 +0100

    First commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Daniel Kuroski on 16.12.20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
